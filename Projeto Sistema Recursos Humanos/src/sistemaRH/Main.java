80cf0a96f9caa25bde5cfad0bc0733ab6d76c9d3
Thiago Melo <poggianth@gmail.com>
2023-06-22 21:21:50 -0300

    Alteração menu

--- Projeto Sistema Recursos Humanos/src/sistemaRH/Main.java
+++ Projeto Sistema Recursos Humanos/src/sistemaRH/Main.java
@@ -1,37 +1,107 @@
 package sistemaRH;
 
+import java.io.IOException;
+import java.util.List;
 import java.util.Scanner;
 
-
+import arquivo.Arquivo;
+import funcionario.Funcionario;
 
 public class Main {
-	static Scanner scan =new Scanner(System.in);
-	public static void main(String[] args) {
+	static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
 		Menus console = new Menus();
-		int matricula;
-		String senha;
-		int aux_loopLogin = 0; 
-		int aux_loopMenu = 0;
-		
-		while(aux_loopLogin != 1) {
-			console.interfaceLogin();
-			System.out.println("Matricula: ");
-			matricula = scan.nextInt();
-			System.out.println("Senha: ");
-			senha = scan.next();
-			
-			// Criar metodo Verificar matricula e senha retornando um boolean -  if true aux_loopLogin = 1	
-			if(matricula == 1234 && senha.equals(senha)){
-				System.out.println("  Acesso efetuado com sucesso!");
-				aux_loopLogin = 1;
-			}
-		}		
-		while(aux_loopMenu != 1) {
-			console.interfaceMenu();
-			aux_loopMenu = 1;
+		int matricula = 1234;
+		String senha = "1234";
+		Arquivo arquivo = new Arquivo();
+		SistemaRH sistemaRh = new SistemaRH();
+
+		// Implementar depois
+		arquivo.verificarArquivo();
+
+		// Interface
+		console.interfaceLogin();
+
+		System.out.print("\nInforme a sua matrícula: ");
+		matricula = scan.nextInt();
+
+		System.out.print("Informe a senha: ");
+		senha = scan.next();
+
+		if (matricula == 1234 && senha.equals("1234")) {
+			int opcao;
+
+			do {
+				console.interfaceMenu();
+				System.out.println("\nInforme a sua opção: ");
+				opcao = scan.nextInt();
+
+				switch (opcao) {
+				case 1: // Inserir funcionário
+					sistemaRh.cadastrarFuncionario();
+					break;
+					
+				case 2: // Atualizar funcionário
+					System.out.println("1 - Atualizar o salário de todos os funcionários");
+					System.out.println("2 - Atualizar o salário por um determinado setor");
+					
+					System.out.print("Informe uma opção: ");
+					int opcaoSalario = scan.nextInt();
+					
+					switch (opcaoSalario) {
+					case 1:
+						break;
+					case 2:
+						break;
+					default:
+						System.err.println("[OPS] - Opção inválida!");
+					}
+					
+					break;
+					
+				case 3: // Remover funcionário
+					break;
+					
+				case 4: // Gerar Relatórios
+					System.out.println("1 - Funcionários acima da Média Salarial");
+					System.out.println("2 - Funcionários por setor");
+					System.out.println("3 - Funcionários por anos de empresa");
+					int opcaoRelatorios = scan.nextInt();
+					
+					switch (opcaoRelatorios) {
+					case 1:
+						break;
+					case 2:
+						List<Funcionario> funcionarios;
+						funcionarios = arquivo.buscarDadosFuncionarios();
+						
+						
+						System.out.println("passou");
+						
+						for(Funcionario func : funcionarios) {
+							System.out.println(func.toString());
+							System.out.println("-----------------");
+						}
+						break;
+					case 3:
+						break;
+					default:
+						System.err.println("[OPS] - Opção inválida!");
+						break;
+					}
+					break;
+					
+				default:
+					System.out.println("[OPÇÃO INVÁLIDA!]");
+				}
+
+			} while (opcao != 0);
+
+		} else {
+			System.err.println("[OPS] - Matrícula ou senha incorretas!");
 		}
-		
-		
+
 	}
 
 }
--- Projeto Sistema Recursos Humanos/src/sistemaRH/SistemaRH.java
+++ Projeto Sistema Recursos Humanos/src/sistemaRH/SistemaRH.java
@@ -1,46 +1,60 @@
 package sistemaRH;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+import arquivo.Arquivo;
 import funcionario.Funcionario;
 
 public class SistemaRH {
-	private List<Funcionario> funcionario;
+	private List<Funcionario> funcionarios;
+	Arquivo arquivo = new Arquivo();
 	
 	public SistemaRH() {
-		this.funcionario = new ArrayList<>();
+		this.funcionarios = new ArrayList<>();
 	}
 	 
 	
 	// Menu de Cadastro de novos Funcionários.
-	public void cadastrarFuncionario() {
+	public void cadastrarFuncionario() throws IOException {
 		
 		// Menu de solicitação de dados do funcionários.
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Cadastro de Funcion�rios:");
 		System.out.println("Informe as seguintes informa��es do novo funcion�rio");
+		
 		System.out.println("Nome: ");
 		String nome = scanner.next();
+		
 		System.out.println("Senha: ");
 		int matricula = scanner.nextInt();
+		
 		System.out.println("Cargo: ");
 		String cargo = scanner.next();
+		
 		System.out.println("Salario: ");
 		double salario = scanner.nextDouble();
+		
 		System.out.println("Data de Adimiss�o: ");
 		String dataContratacao = scanner.next();
+		
 		System.out.println("Setor: ");
 		String setor = scanner.next();
 		
-		scanner.close();
 		
 		// Criando um novo funcionário com os dados solicitados.
-		Funcionario funcionario = new Funcionario( 0, nome, matricula, cargo, salario, dataContratacao, setor);
+		Funcionario funcionario = new Funcionario(0, nome, matricula, cargo, salario, dataContratacao, setor);
 		// Adicionando o funcionário criado no ArrayList funcionario.
-		this.funcionario.add(funcionario);
-		System.out.println("O novo funcionário " + funcionario + " foi criado com sucesso!");	
+		
+		if(arquivo.gravarDadosFuncionario(funcionario.toString())) {			
+			this.funcionarios.add(funcionario);
+			System.out.println("O novo funcionário " + funcionarios + " foi criado com sucesso!");
+		} else {
+			System.err.println("[OPS] - Erro ao cadastrar um novo funcionário!");
+		}
+		
 		
 	}
 	
